03c53a73cfc645a04f53c4521d707691d96d3d6a
Tim Maffett <timmaffett@gmail.com>
2022-03-22 16:30:37 -0700

    added build_minified, clean_minified, and clean_node, add summary header to codemirror.js output file, add more default includes

--- tool/grind.dart
+++ tool/grind.dart
@@ -6,15 +6,37 @@ import 'dart:io';
 
 import 'package:grinder/grinder.dart';
 
-final Directory srcDir = Directory('third_party/codemirror');
+final String cm_minified_dirname = 'codemirror_minified';
+final Directory srcMinifiedDir =
+    joinDir(Directory('third_party'), [cm_minified_dirname]);
+Directory srcDir = Directory('third_party/codemirror');
 final Directory destDir = Directory('lib');
 
 Future main(List<String> args) => grind(args);
 
+@Task(
+    'Minify the codemirror files, and then proceed with "build" using minified codemirror\n--verbose shows intermediate output')
+@Depends(clean_minified)
+void build_minified() {
+  TaskArgs args = context.invocation.arguments;
+  bool verbose = args.getFlag('verbose');
+  RunOptions runOpts = RunOptions(
+      workingDirectory: 'third_party', includeParentEnvironment: true);
+  // make sure google closure compiler is there
+  run('npm', arguments: ['install'], quiet: !verbose, runOptions: runOpts);
+
+  // run the gulpfile.js and minify codemirror into codemirror_minified
+  run('gulp', quiet: !verbose, runOptions: runOpts);
+
+  // OK we can go ahead and build like normal, but using the minified source
+  srcDir = srcMinifiedDir;
+  build();
+}
+
 @Task('Copy the codemirror files from third_party/ into lib/')
 void build() {
   // Copy codemirror.js.
-  var jsSource = _concatenateModes(srcDir);
+  var jsSource = _concatenateModesAndOtherDependencies(srcDir);
   joinFile(destDir, ['codemirror.js']).writeAsStringSync(jsSource);
   //copy(joinFile(srcDir, ['lib', 'codemirror.js']), destDir);
 
@@ -43,10 +65,28 @@ void test() {
 void clean() {
   delete(joinFile(destDir, ['codemirror.js']));
   delete(joinFile(destDir, ['css', 'codemirror.css']));
-  delete(joinFile(destDir, ['theme']));
+  delete(joinDir(destDir, ['addon']));
+  delete(joinDir(destDir, ['keymap']));
+  delete(joinDir(destDir, ['mode']));
+  delete(joinDir(destDir, ['theme']));
 }
 
-String _concatenateModes(Directory dir) {
+@Task('Delete all generated minified codemirror')
+void clean_minified() {
+  delete(srcMinifiedDir);
+}
+
+@Task('Delete all node modules used by minifier,build_minified will reinstall')
+void clean_node() {
+  final Directory thirdPartyDir = Directory('third_party');
+  delete(joinDir(thirdPartyDir, ['node_modules']));
+  delete(joinFile(thirdPartyDir, ['package-lock.json']));
+}
+
+String _concatenateModesAndOtherDependencies(Directory dir) {
+  TaskArgs args = context.invocation.arguments;
+  bool legacyAddons = args.getFlag('legacyaddons');
+
   var files = <File>[];
 
   // Read lib/codemirror.js.
@@ -70,15 +110,29 @@ String _concatenateModes(Directory dir) {
 
   // Add an API to add a panel above or below the editor.
   files.add(joinFile(dir, ['addon', 'display', 'panel.js']));
-  files.add(joinFile(dir, ['addon', 'scroll', 'simplescrollbars.js']));
 
   // Add search addons.
-  files.add(joinFile(dir, ['addon', 'scroll', 'simplescrollbars.js']));
   files.add(joinFile(dir, ['addon', 'search', 'search.js']));
   files.add(joinFile(dir, ['addon', 'search', 'searchcursor.js']));
-  files.add(joinFile(dir, ['addon', 'scroll', 'annotatescrollbar.js']));
-  files.add(joinFile(dir, ['addon', 'search', 'matchesonscrollbar.js']));
-  files.add(joinFile(dir, ['addon', 'search', 'match-highlighter.js']));
+
+  if (!legacyAddons) {
+    // used on all dart-pads
+    files.add(joinFile(dir, ['addon', 'scroll', 'simplescrollbars.js']));
+
+    // search dialog
+    files.add(joinFile(dir, ['addon', 'scroll', 'annotatescrollbar.js']));
+    files.add(joinFile(dir, ['addon', 'search', 'matchesonscrollbar.js']));
+    files.add(joinFile(dir, ['addon', 'search', 'match-highlighter.js']));
+    // code folding
+    files.add(joinFile(dir, ['addon', 'fold', 'foldcode.js']));
+    files.add(joinFile(dir, ['addon', 'fold', 'foldgutter.js']));
+    files.add(joinFile(dir, ['addon', 'fold', 'brace-fold.js']));
+    files.add(joinFile(dir, ['addon', 'fold', 'xml-fold.js']));
+    files.add(joinFile(dir, ['addon', 'fold', 'indent-fold.js']));
+    files.add(joinFile(dir, ['addon', 'fold', 'comment-fold.js']));
+    // html tag matching
+    files.add(joinFile(dir, ['addon', 'edit', 'matchtags.js']));
+  }
 
   // Required by some modes.
   files.add(joinFile(dir, ['addon', 'mode', 'overlay.js']));
@@ -103,16 +157,35 @@ String _concatenateModes(Directory dir) {
   files.add(joinFile(dir, ['addon', 'dialog', 'dialog.js']));
   files.add(joinFile(dir, ['keymap', 'vim.js']));
 
-
 //  var modeFiles = joinDir(dir, ['mode'])
 //    .listSync()
 //    .where((dir) => dir is Directory)
 //    .map((dir) => joinFile(dir, ['${fileName(dir)}.js']))
 //    .where((f) => f.existsSync());
 //  files.addAll(modeFiles);
-
-  return files.map((File file) {
-    var header = '// ${fileName(file)}\n\n';
-    return header + file.readAsStringSync().trim() + '\n';
+  int count = 0;
+  String topHeaderFileList = files.map((File file) {
+    String filenameCommentForHeader;
+    if (count++ == 0) {
+      // codemirror file
+      filenameCommentForHeader = '// ${fileName(file)}';
+    } else {
+      // addons
+      List<String> fileparts = file.path.split('/');
+      int len = fileparts.length;
+      filenameCommentForHeader = '//    ' +
+          (len >= 3 && (fileparts[len - 3] != cm_minified_dirname)
+              ? fileparts[len - 3] + '/'
+              : '') +
+          (len >= 2 ? fileparts[len - 2] + '/' : '') +
+          fileparts[len - 1];
+    }
+    return filenameCommentForHeader;
   }).join('\n');
+
+  return '$topHeaderFileList\n\n' +
+      files.map((File file) {
+        var header = '// ${fileName(file)}\n\n';
+        return header + file.readAsStringSync().trim() + '\n';
+      }).join('\n');
 }
